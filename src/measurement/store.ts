d0a53d1eecdf240b2b3bbbbb0b78d48848b22680
Alexey Yarmosh <yarmoshalexey@gmail.com>
2022-11-09 11:33:46 +0300

    feat: use redis.multi() instead of Promise.all

--- src/measurement/store.ts
+++ src/measurement/store.ts
@@ -27,76 +27,72 @@ export class MeasurementStore {
 		const key = getMeasurementKey(id);
 
 		const probesAwaitingTtl = config.get<number>('measurement.timeout') + 5;
-
-		await Promise.all([
-			// eslint-disable-next-line @typescript-eslint/naming-convention
-			this.redis.set(getMeasurementKey(id, 'probes_awaiting'), probesCount, {EX: probesAwaitingTtl}),
-			this.redis.json.set(key, '$', {
-				id,
-				type: test.type,
-				status: 'in-progress',
-				createdAt: Date.now(),
-				updatedAt: Date.now(),
-				probesCount,
-				results: {},
-			}),
-			this.redis.expire(key, config.get<number>('measurement.resultTTL')),
-		]);
+		const multi = this.redis.multi();
+		// eslint-disable-next-line @typescript-eslint/naming-convention
+		multi.set(getMeasurementKey(id, 'probes_awaiting'), probesCount, {EX: probesAwaitingTtl});
+		multi.json.set(key, '$', {
+			id,
+			type: test.type,
+			status: 'in-progress',
+			createdAt: Date.now(),
+			updatedAt: Date.now(),
+			probesCount,
+			results: {},
+		});
+		multi.expire(key, config.get<number>('measurement.resultTTL'));
+		await multi.exec();
 
 		return id;
 	}
 
 	async storeMeasurementProbe(measurementId: string, probeId: string, probe: Probe): Promise<void> {
 		const key = getMeasurementKey(measurementId);
-		await Promise.all([
-			this.redis.json.set(key, `$.results.${probeId}`, {
-				probe: {
-					continent: probe.location.continent,
-					region: probe.location.region,
-					country: probe.location.country,
-					state: probe.location.state ?? null,
-					city: probe.location.city,
-					asn: probe.location.asn,
-					longitude: probe.location.longitude,
-					latitude: probe.location.latitude,
-					network: probe.location.network,
-					resolvers: probe.resolvers,
-				},
-				result: {rawOutput: ''},
-			}),
-			this.redis.json.set(key, '$.updatedAt', Date.now()),
-		]);
+		const multi = this.redis.multi();
+		multi.json.set(key, `$.results.${probeId}`, {
+			probe: {
+				continent: probe.location.continent,
+				region: probe.location.region,
+				country: probe.location.country,
+				state: probe.location.state ?? null,
+				city: probe.location.city,
+				asn: probe.location.asn,
+				longitude: probe.location.longitude,
+				latitude: probe.location.latitude,
+				network: probe.location.network,
+				resolvers: probe.resolvers,
+			},
+			result: {rawOutput: ''},
+		});
+		multi.json.set(key, '$.updatedAt', Date.now());
+		await multi.exec();
 	}
 
 	async storeMeasurementProgress(data: MeasurementResultMessage): Promise<void> {
 		const key = getMeasurementKey(data.measurementId);
+		const multi = this.redis.multi();
 
-		await Promise.all([
-			data.overwrite
-				? this.redis.json.set(key, `$.results.${data.testId}.result.rawOutput`, data.result.rawOutput)
-				: this.redis.json.strAppend(key, `$.results.${data.testId}.result.rawOutput`, data.result.rawOutput),
-
-			this.redis.json.set(key, '$.updatedAt', Date.now()),
-		]);
+		data.overwrite
+			? multi.json.set(key, `$.results.${data.testId}.result.rawOutput`, data.result.rawOutput)
+			: multi.json.strAppend(key, `$.results.${data.testId}.result.rawOutput`, data.result.rawOutput);
+		multi.json.set(key, '$.updatedAt', Date.now());
+		await multi.exec();
 	}
 
 	async storeMeasurementResult(data: MeasurementResultMessage): Promise<void> {
 		const key = getMeasurementKey(data.measurementId);
-
-		await Promise.all([
-			this.redis.json.set(key, `$.results.${data.testId}.result`, data.result),
-			this.redis.json.set(key, '$.updatedAt', Date.now()),
-			this.redis.decr(`${key}:probes_awaiting`),
-		]);
+		const multi = this.redis.multi();
+		multi.json.set(key, `$.results.${data.testId}.result`, data.result);
+		multi.json.set(key, '$.updatedAt', Date.now());
+		multi.decr(`${key}:probes_awaiting`);
+		await multi.exec();
 	}
 
 	async markFinished(id: string): Promise<void> {
 		const key = getMeasurementKey(id);
-
-		await Promise.all([
-			this.redis.json.set(key, '$.status', 'finished'),
-			this.redis.json.set(key, '$.updatedAt', Date.now()),
-		]);
+		const multi = this.redis.multi();
+		multi.json.set(key, '$.status', 'finished');
+		multi.json.set(key, '$.updatedAt', Date.now());
+		await multi.exec();
 	}
 }
 
